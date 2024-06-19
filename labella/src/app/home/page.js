d8a826d110bb3ce9c174b841275d12b87e496a2d
olivia <111635889+oliviaow2022@users.noreply.github.com>
2024-06-19 09:42:02 +0800

    create project page wip

--- labella/src/app/home/page.js
+++ labella/src/app/home/page.js
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 export default function Home() {
     return (
-        <main className="flex flex-col min-h-screen px-24 pb-24 bg-[#19151E] z-20 justify-center">
-            <div className="flex flex-row fixed top-0 h-24 w-11/12 z-20 bg-[#19151E] items-end">
+        <main className="flex flex-col min-h-screen px-24 pb-24 bg-[#19151E] z-20">
+            <div className="flex flex-row fixed top-0 h-24 w-10/12 2xl:w-full z-20 bg-[#19151E] items-end">
                 <div className="z-10 max-w-5xl w-full justify-between text-sm lg:flex">
                     <Link href="/"><p className="text-xl font-bold">Labella</p></Link>
                 </div>
--- labella/src/app/image-classification/page.js
+++ labella/src/app/image-classification/page.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Link from 'next/link';
+import InputBox from "@/components/InputBox";
 
 export default function ImageClassification() {
-    const menuOptions = ["Model", "Details and Dataset", "Label", "Fine Tune", "Performance and Statistics"]
-    const models = ["model 1", "model 2", "model 3", "model 4", "model 5", "model 6"]
+    const menuOptions = ["Project Details", "Model", "Dataset", "Label", "Fine Tune", "Performance and Statistics"]
+    const models = ["Resnet18", "Densenet121", "Alexnet", "Convnext_base"]
     return (
         <main className="flex flex-col min-h-screen px-24 pb-24 bg-[#19151E] z-20">
-            <div className="flex flex-row fixed top-0 h-24 w-full z-20 bg-[#19151E] items-end">
+            <div className="flex flex-row fixed top-0 h-24 w-10/12 2xl:w-full z-20 bg-[#19151E] items-end">
                 <div className="z-10 max-w-5xl w-full justify-between text-sm lg:flex">
                     <Link href="/"><p className="text-xl font-bold">Labella</p></Link>
                 </div>
@@ -22,44 +23,58 @@ export default function ImageClassification() {
                         <p key={index} className="hover:cursor-pointer hover:text-[#FF52BF]">{option}</p>
                     ))}
                 </div>
-                <div className="h-screen bg-white border-2 mt-10 ml-64"></div>
+                <div className="bg-white border-2 mt-32 ml-64 h-screen sticky"></div>
                 <div className="ml-20">
-                    <p className="text-xl text-[#FF52BF] font-bold my-10">Image Classification</p>
-                    <p className="font-bold mb-10">Select Model</p>
-                    <div className="grid grid-cols-4 gap-8">
-                        {models.map((model, index) => (
-                            <div key={index} className="flex border w-64 items-center justify-center rounded-lg h-10 hover:bg-[#FF52BF] cursor-pointer hover:text-black">
-                                {model}
-                            </div>
-                        ))}
+                    <p className="text-xl text-[#FF52BF] font-bold mb-8 mt-40">Image Classification</p>
+                    <div className="mb-4">
+                        <p className="font-bold mb-4">Create a new project</p>
+                        <InputBox label={"Project Name"} />
                     </div>
-                    <p className="font-bold mt-10 mb-4">Provide details for your data labelling</p>
-                    <div className="flex flex-col gap-4">
-                        <div>
-                            <p className="mb-2">Selected Model</p>
 
-                            <div className="flex border w-64 rounded-full pl-4 py-1">Model 1</div>
+                    <div className="mb-4">
+                        <p className="mt-4">Select Model</p>
+                        <div className="grid grid-cols-4 gap-6">
+                            {models.map((model, index) => (
+                                <div key={index} className="flex border w-40 2xl:w-64 items-center justify-center rounded-lg h-8 hover:bg-[#FF52BF] cursor-pointer hover:text-black">
+                                    {model}
+                                </div>
+                            ))}
                         </div>
-                        <div>
-                            <p className="mb-2">Choose Dataset</p>
-                            <div className="flex border w-64 rounded-full pl-4 py-1 items-center">
-                                <img src="/upload.png" alt="upload icon" className="h-4 mr-2"></img>
-                                <p>Upload Dataset</p>
-                            </div>
-                        </div>
-                        <div>
-                            <p className="mb-2">Number of classes</p>
-                            <div className="flex border w-64 rounded-full pl-4 py-1 items-center">
-                                10
-                            </div>
-                        </div>
-                        <div>
-                            <div className="flex bg-[#FF52BF] w-20 rounded-full justify-center items-center cursor-pointer">
-                                <p>Label</p>
-                            </div>
-                        </div>
-
                     </div>
+
+                    <div className="mb-4">
+                        <InputBox label={"Number of training epochs"} />
+                    </div>
+
+                    <div className="mb-4">
+                        <InputBox label={"Train-test split ratio"} />
+                    </div>
+
+                    <div className="mb-4">
+                        <InputBox label={"Batch size"} />
+                    </div>
+
+
+                    <div className="mb-4">
+                        <p className="">Choose Dataset</p>
+                        <div className="flex border w-64 rounded-lg pl-4 py-1 items-center">
+                            <img src="/upload.png" alt="upload icon" className="h-4 mr-2"></img>
+                            <p>Upload Dataset</p>
+                        </div>
+                    </div>
+                    <div className="mb-4">
+                        <p className="mb-2">Number of classes</p>
+                        <div className="flex border w-64 rounded-lg pl-4 py-1 items-center">
+                            10
+                        </div>
+                    </div>
+                    <div>
+                        <div className="flex bg-[#FF52BF] w-32 rounded-full justify-center items-center cursor-pointer">
+                            <p>Create Project</p>
+                        </div>
+                    </div>
+
+
 
                 </div>
             </div>
