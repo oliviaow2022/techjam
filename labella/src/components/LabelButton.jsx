27a637e25f6e4c524552d6ee3a8177a36793f878
olivia <111635889+oliviaow2022@users.noreply.github.com>
2024-07-28 01:06:40 +0800

    label for multiclass image

--- labella/src/components/LabelButton.jsx
+++ labella/src/components/LabelButton.jsx
@@ -1,11 +1,23 @@
 'use client'
 import React, { useState } from 'react';
 
-export default function LabelButton({ classInteger, name, handleOptionChange, bgColour = 'bg-[#FF52BF]', selectedOption}) {
+export default function LabelButton({classInteger, name, handleOptionChange, bgColour = 'bg-[#FF52BF]', selectedOption, isMulticlass}) {
     const label = selectedOption?.labels
+    let match = false
+    if (isMulticlass){
+        const list = classInteger.split("_")
+        const categoryIndex = parseInt(list[0])
+        const categoryInteger = parseInt(list[1])
+        if(label != null && (parseInt(label[categoryIndex]) === categoryInteger)){
+            match = true
+        }
+    } else if(label == classInteger){
+        match = true
+    }
+
     return (
         <div onClick={() => handleOptionChange(classInteger)}>
-            <button htmlFor="input-box" className={`mb-2 ${label == classInteger ? "bg-[#FF52BF] text-black" : "bg-black text-white"} border border-white px-4 py-2 rounded-full hover:${bgColour} hover:text-black`}>
+            <button htmlFor="input-box" className={`mb-2 ${match ? "bg-[#FF52BF] text-black" : "bg-black text-white"} border border-white px-4 py-2 rounded-full hover:${bgColour} hover:text-black`}>
                 {name}
             </button>
         </div>
