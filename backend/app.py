def1462402078d7445f7aff9869f0cb73610ec2a
Sharlene Tio <sharlenetio.2021@scis.smu.edu.sg>
2024-07-31 22:59:52 +0800

    fix sentiment analysis create redirect

--- backend/app.py
+++ backend/app.py
@@ -93,54 +93,45 @@ def hello_world():
 @flask_app.cli.command('seed')
 @with_appcontext
 def seed():
-   db.drop_all()
-   db.create_all()
+    db.drop_all()
+    db.create_all()
 
+    """Seed the database."""
+    user = User(email='test@gmail.com', username='testuser')
+    user.set_password('testuser')
+    db.session.add(user)
+    db.session.commit()
 
-   """Seed the database."""
-   user = User(email='test@gmail.com', username='testuser')
-   user.set_password('testuser')
-   db.session.add(user)
-   db.session.commit()
+    ants_bees = Project(name="Multi-Class Classification", user_id=user.id, bucket=os.getenv('S3_BUCKET'), prefix='transfer-antsbees', type="Single Label Classification")
+    db.session.add(ants_bees)
+    db.session.commit()
 
+    ants_bees_ds = Dataset(name="Ants and Bees", project_id=ants_bees.id, num_classes=2, class_to_label_mapping={0: 'ants', 1: 'bees'})
+    db.session.add(ants_bees_ds)
+    db.session.commit()
 
-   ants_bees = Project(name="Multi-Class Classification", user_id=user.id, bucket=os.getenv('S3_BUCKET'), prefix='transfer-antsbees', type="Single Label Classification")
-   fashion_mnist = Project(name="Fashion Mnist", user_id=user.id, bucket=os.getenv('S3_BUCKET'), prefix='fashion-mnist', type="Single Label Classification")
-   cifar10 = Project(name="Cifar-10", user_id=user.id, type="1")
-   db.session.add_all([ants_bees, fashion_mnist, cifar10])
-   db.session.commit()
+    resnet18 = Model(name='ResNet-18', project_id=ants_bees.id)
+    densenet121 = Model(name='DenseNet-121', project_id=ants_bees.id)
+    alexnet = Model(name='AlexNet', project_id=ants_bees.id)
+    convnext_base = Model(name='ConvNext Base', project_id=ants_bees.id)
+    db.session.add_all([resnet18, densenet121, alexnet, convnext_base])
+    db.session.commit()
 
+    """Seed the database from a CSV file."""
+    file_path = 'dataset.csv'
+    with open(file_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data_instance = DataInstance(
+                data=row['filename'],
+                labels=row['label'],
+                dataset_id=1,
+                manually_processed=bool(row.get('manually_processed', False))
+            )
+            db.session.add(data_instance)
+        db.session.commit()
 
-   ants_bees_ds = Dataset(name="Ants and Bees", project_id=ants_bees.id, num_classes=2, class_to_label_mapping={0: 'ants', 1: 'bees'})
-   db.session.add(ants_bees_ds)
-   db.session.commit()
-
-
-   resnet18 = Model(name='ResNet-18', project_id=ants_bees.id)
-   densenet121 = Model(name='DenseNet-121', project_id=ants_bees.id)
-   alexnet = Model(name='AlexNet', project_id=ants_bees.id)
-   convnext_base = Model(name='ConvNext Base', project_id=ants_bees.id)
-   resnet18_2 = Model(name='ResNet-18', project_id=fashion_mnist.id)
-   resnet18_3 = Model(name='ResNet-18', project_id=cifar10.id)
-   db.session.add_all([resnet18, densenet121, alexnet, convnext_base, resnet18_2, resnet18_3])
-   db.session.commit()
-
-
-   """Seed the database from a CSV file."""
-   file_path = 'dataset.csv'
-   with open(file_path, newline='') as csvfile:
-       reader = csv.DictReader(csvfile)
-       for row in reader:
-           data_instance = DataInstance(
-               data=row['filename'],
-               labels=row['label'],
-               dataset_id=1,
-               manually_processed=bool(row.get('manually_processed', False))
-           )
-           db.session.add(data_instance)
-       db.session.commit()
-
-   click.echo('Seed data added successfully.')
+    click.echo('Seed data added successfully.')
 
 
 if __name__ == "__main__":
